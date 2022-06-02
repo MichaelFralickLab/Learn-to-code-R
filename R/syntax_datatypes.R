9d31282f114b991cd67376bf87b5c4769cb7ebec
jmoggridge <ja.moggridge@gmail.com>
2022-06-01 22:19:15 -0700

    last changes

--- R/syntax_datatypes.R
+++ R/syntax_datatypes.R
@@ -1,3 +1,5 @@
+# https://raw.githubusercontent.com/jmoggridge/Learn-to-code-R/main/R/syntax_datatypes.R
+
 ##################################################
 ## Project: R syntax and datatypes. My first R script!
 ##################################################
@@ -8,15 +10,17 @@
 #' it's a brief intro to the things we'll be exposed to later.
 #'
 #' These examples are simple to expose the basic mechanics of R.
-#' Don't follow along here just focus, the examples are short and simple.
+#' Don't follow along here, just focus, the examples are short and simple.
 #'
 ##################################################
 
 ##----------------------------------------------##
 #' 1. assign an object to a name with <-
 ##----------------------------------------------##
-# name <- object
-# use keyboard shortcut for <- : 'alt' and '-'
+# name <- expression
+# use keyboard shortcut for <- :
+# 'alt' and '-'  on windows
+# 'option' and '-' on Mac
 
 # store a value
 my_value <- 5
@@ -48,16 +52,13 @@ date_val <- Sys.Date()
 missing_val <- NA
 nothing <- NULL
 
-
-#' Check out the environment panel list and grid views
-# there are other specific types but this is most of what we'll encounter
-
-# clean up the workspace...
+#' Check out the environment panel, both 'list' and 'grid' views
+# now let's clear out all the objects we created (like the broom button)
 rm(list = ls())
 
 #' restart R with 'cmd + shft + 0'
-#
 
+##----------------------------------------------##
 #' coerce data from one class to another
 ##----------------------------------------------##
 fct_val <- factor('category1')
@@ -71,10 +72,7 @@ as.logical(fct_val)
 as.numeric(fct_val)
 as.logical(as.numeric(fct_val))
 
-
-
-
-
+##----------------------------------------------##
 #' we can do all the **math** you want
 ##----------------------------------------------##
 # + - * / ** // log exp sin cos tan  %/% %% ....
@@ -82,6 +80,7 @@ as.logical(as.numeric(fct_val))
 9 %/% 2
 9 %% 2
 
+##----------------------------------------------##
 #' ...or **boolean** algebra
 ##----------------------------------------------##
 # | 'or'
@@ -89,7 +88,6 @@ as.logical(as.numeric(fct_val))
 # ! 'not'
 a <- T
 b <- F
-a | b      # true
 
 a | b      # true
 a & b      # false
@@ -99,7 +97,7 @@ a & b      # false
 (a | b) & !(a & b)
 
 # 'xor': one or the other is true but not both
-base::xor |> View()
+xor(T, F)
 
 # xor <- function(x, y){
 #   # cat("powered by Jason\n")
@@ -115,7 +113,7 @@ xor(F, F)
 # congrats, you've just learned boolean algebra
 
 
-
+##----------------------------------------------##
 #' **vectors**
 ##----------------------------------------------##
 # any single value is already a vector...
@@ -124,7 +122,10 @@ print('some text')
 # the [1] indicates the index of our value.
 # our object named chr_val is a vector of length 1.
 is.vector('some text')
+is.vector(1)
 length('some text')
+nchar('some text')
+
 
 # use is.* functions to test
 is.vector('some text')
@@ -154,7 +155,8 @@ paste('number:', my_vector)
 
 #' *subset with indices (generally discouraged)*
 ##----------------------------------------------##
-another_vector <- letters[1:10]
+those_are_useful_for_xxx <- 1:10
+another_vector <- letters[those_are_useful_for_xxx]
 
 
 #' *vectors can't have different types of data*
@@ -164,31 +166,61 @@ print(my_vector)
 class(my_vector)
 as.numeric(my_vector)
 
-#' *but lists can store all types of data, including vectors & lists
 ##----------------------------------------------##
-my_list <- list(group = 'A',
-                length = 1.5769,
-                treatment = T,
-                dose = 30
-                parents = c('Bob', 'Alice')
-                )
-print(my_list)
-
-#' but data.frames are better if data is a table
+#' *lists can store all types of data, including other lists
 ##----------------------------------------------##
-my_df <- data.frame(group = 'A',  length = 1.5769, treatment = T, dose = 30)
-print(my_df)
+patient <- list(
+  group = 'A',
+  weight = 98,
+  age = 58,
+  treatment = T,
+  dose = 30,
+  parents = list('Bob', 'Alice')
+)
+print(patient)
+str(patient)
 
+# access values by name
+dose <- patient$dose
 
+# increase dose
+dose + 5
 
+# or position
+dose2 <- patient[5][[1]]
+class(dose2)
 
+##----------------------------------------------##
+## Data.frames
+##----------------------------------------------##
+#' data.frame is preferred over list if possible
+patient <- as.data.frame(patient)
+str(patient)
 
+#' a table with one row
+patient <- data.frame(
+  group = 'A',
+  weight = 98,
+  age = 58,
+  treatment = T,
+  dose = 30
+)
 
+# get (first) value of dose column
+patient$dose[1]
+patient[1, 'dose']
 
+# A better data structure is the 'tibble' from tidyverse
+# it allows you to work with columns with lists
+tibble::tibble(
+  group = 'A',
+  weight = 98,
+  age = 58,
+  treatment = T,
+  dose = 30,
+  parents = list(c('Alice', 'Bob'))
+) |>
+  tibble::glimpse()
 
-
-
-
-
-
-
+as.data.frame(patient)
+tibble::as_tibble(patient)
